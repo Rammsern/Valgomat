024345790f574a88d0865acca2ee42469284e2ae
Adrian <Adrian.shope@icloud.com>
2025-09-15 08:52:56 +0200

    Definerte spørsmål array i app.js

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const QUESTIONS = [
-  {id:'q1', text:'Privat sektor bør utgjøre en større andel av økonomien.'},
-  {id:'q2', text:'Norge bør slutte å lete etter nye oljefelt.'}
+{id: 'q1', cat:'økonomi', text:'privat sektor bør utjøre en større andel av økonomien.'},
+{id: 'q2', cat:'klima', text:'Norge bør slutte å lete etter nye oljefelt.'},
+{id: 'q3', cat:'velferd', text:'velferdsordningene bør bevares på dagens nivå.'},
 ];
+console.log("spørsmål lastet:", questions);
 
-document.getElementById("startBtn").addEventListener("click", () => {
-  showQuestion(0);
-});
-
-function showQuestion(index) {
-  const q = QUESTIONS[index];
-  const questionArea = document.getElementById("questionArea");
-  questionArea.innerHTML = `<h2>${q.text}</h2>`;
-}
