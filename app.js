87e087d2fd7bef1fc6c491fb96dd93abeeecef7f
Adrian <Adrian.shope@icloud.com>
2025-09-16 12:31:35 +0200

    koden er ferdig og nettsiden er brukbar og alt funker som den skal
    det var en del issues som jeg fant ut av nå. F. eks at jeg ikke fikk til å vise rusultat eller velge svar. Det var bare at jeg måtte adde en kode som ikke var defined som var let answers = [];. etter det så funket alt som det skal

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const QUESTIONS = [
-{id: 'q1', cat:'økonomi', text:'privat sektor bør utjøre en større andel av økonomien.'},
+{id: 'q1', cat:'økonomi', text:'privat sektor bør utgjøre en større andel av økonomien.'},
 {id: 'q2', cat:'klima', text:'Norge bør slutte å lete etter nye oljefelt.'},
 {id: 'q3', cat:'velferd', text:'velferdsordningene bør bevares på dagens nivå.'},
 ];
 console.log("spørsmål lastet:", QUESTIONS);
+
+let answers = [];
+
 
 const startBtn = document.getElementById("startBtn");
 const questionArea = document.getElementById("questionArea");
@@ -25,11 +28,11 @@ function showQuestion(index) {
     questionArea.innerHTML = `
     <><h2>${q.cat}</h2><p>${q.text}</p></>
     <div class="options">
-    <button class="option" data-value="-2">Helt uenig</button>
-    <button class="option" data-value="-1">litt uenig</button>
+    <button class="option" data-value="0">Helt uenig</button>
+    <button class="option" data-value="0">litt uenig</button>
     <button class="option" data-value="0">nøytral</button>
-    <button class="option" data-value="1">litt enig</button>
-    <button class="option" data-value="2">Helt enig</button>
+    <button class="option" data-value="5">litt enig</button>
+    <button class="option" data-value="10">Helt enig</button>
     </div>
     <button id="nextBtn">${index < QUESTIONS.length-1 ? "neste" : "se resultat"}</button>
     `;
@@ -43,7 +46,7 @@ document.getElementById("nextBtn").addEventListener("click", () => {
 });
 }
 questionArea.addEventListener("click", (e) => {
-    if(e.target.classlist.contains("option")){
+    if(e.target.classList.contains("option")){
         const value = parseInt(e.target.dataset.value);
         const qid = QUESTIONS[currentIndex].id;
         answers[qid] = value;
@@ -59,14 +62,14 @@ function showResult() {
 
     QUESTIONS.forEach(q => {
         if(answers[q.id] !== undefined) {
-            sum += answers[q.id];
+            sum += answers[q.id] * 10;
             count++;
         }
     });
 const avg = count > 0 ? (sum / count) : 0;
 questionArea.innerHTML = `
 <h2>Resultat</h2>
-<p>gjennomsnittlig enighet: ${avg.toFixed(2)}</p>
+<p>gjennomsnittlig enighet: ${avg}%</p>
 `;
 }
 
